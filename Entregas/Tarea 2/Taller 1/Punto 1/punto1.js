944994ec1e07c9b9d9072b3abb8502bdc84c8f63
Julian Alexander Murillo <julixrevolutions@gmail.com>
2013-08-17 12:51:39 -0500

    Se pasa de JavaScript puro a jQuery junto con la importación de las librerías

--- Entregas/Tarea 2/Taller 1/Punto 1/punto1.js
+++ Entregas/Tarea 2/Taller 1/Punto 1/punto1.js
@@ -1,13 +1,49 @@
 function adicionarCancion(){
-	var ul = document.getElementById("listaCanciones");
-	var li = document.createElement("li");
+	var texto = $("#nombreCancion").val();
+	var puedeAgregar = true;
 
-	li.className = "cancion";
-	li.textContent = document.getElementById("nombreCancion").value;
-	ul.appendChild(li);
+	/* 
+	 * Each: Ciclo en jQuery
+	 * Parametro: k (key)
+	 * Parametro: v (value)
+	 */
+	$(".cancion").each(function(k, v) {
+		var actualCancion = $(this).text();
+
+		if(actualCancion.toLowerCase() == texto.toLowerCase()) {
+			puedeAgregar = false;
+		}
+	});
+
+	if(texto != "" && puedeAgregar) {
+		var nuevoLi = $("<li>");
+		nuevoLi.addClass("cancion");
+		nuevoLi.text(texto);
+		$("#listaCanciones").append(nuevoLi);	
+	}
+
+	// Limpia la entrada con el id nombreCancion
+	$("#nombreCancion").val("")
 }
 
-window.onload = function() {
-	var btnAdicionarCancion = document.getElementById("adicionarCancion");
-	btnAdicionarCancion.onclick = adicionarCancion;
-}
+/* 
+ * Se ejecuta cuando el documento este listo
+ * (equivalente a window.onload)
+ */
+$(document).ready(function(e) {
+	$("#adicionarCancion").click(function() {
+		adicionarCancion();
+	});
+});
+
+/*
+ * Se ejecuta cuando es presionada una
+ * tecla.
+ */
+$(document).keypress(function(evt){
+
+	// La tecla 13 es 'enter' en el teclado
+	if (evt.keyCode == 13) {
+    	adicionarCancion();
+    }	
+})
